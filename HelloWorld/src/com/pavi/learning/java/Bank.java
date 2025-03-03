9b232ebd5b688e61875e81487c8642734fa1d2d4
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 12:50:37 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/Bank.java
@@ -0,0 +1,25 @@
+package com.pavi.learning.java.jdbc;
+
+import java.time.LocalDateTime;
+
+public class Bank {
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
