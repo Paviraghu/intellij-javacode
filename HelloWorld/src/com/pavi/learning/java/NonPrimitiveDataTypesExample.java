bb4c1aff947c9ff925dd76c225bcd944b69f09d0
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 12:36:29 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/NonPrimitiveDataTypesExample.java
@@ -0,0 +1,17 @@
+package com.pavi.learning.java.DataTypes;
+
+public class NonPrimitiveDataTypesExample {
+
+    public static void main(String[] args) {
+
+        String name = "John Doe";
+        System.out.println("String value:" + name);
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array value at index 2:" + numbers[2]);
+
+        System.out.println("Array length : " + numbers.length);
+
+    }
+}
