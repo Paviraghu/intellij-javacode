5280a0bcb7efb142b4729a5570b12932051a9ea9
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 13:07:15 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/B.java
@@ -0,0 +1,9 @@
+package com.pavi.learning.java.p2;
+import com.pavi.learning.java.p1.A;
+
+ public class B {
+    public static void main(String[] args){
+        A obj = new A();
+        obj.display();
+    }
+}
