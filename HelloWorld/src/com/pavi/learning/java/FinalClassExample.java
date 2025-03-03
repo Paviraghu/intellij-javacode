d88d587a4171539ac0a3793292327829238acd32
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 12:47:01 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/FinalClassExample.java
@@ -0,0 +1,16 @@
+package com.pavi.learning.java.finalexample;
+
+final class FinalClass {
+
+    void display(){
+        System.out.println("This is a final class");
+    }
+}
+public class FinalClassExample{
+
+    public static void main(String[] args){
+        FinalClass obj = new FinalClass();
+        obj.display();
+
+    }
+}
