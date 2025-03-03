1a62d853c6f0e136e5b92edae496a2b8cb30bf6f
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 13:06:32 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/A.java
@@ -0,0 +1,15 @@
+package com.pavi.learning.java.p1;
+
+public class A{
+    public void display() {
+        System.out.println("Mahitech");
+    }
+}
+
+class B{
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.display();
+
+    }
+}
