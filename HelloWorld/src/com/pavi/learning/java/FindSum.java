90fc61fa394222794b60b9bd7604d9314530b28c
Paviraghu <paviraghu5410@gmail.com>
2025-03-03 12:34:48 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/FindSum.java
@@ -0,0 +1,35 @@
+package com.pavi.learning.java.datastructures;
+
+public class FindSum {
+
+    public static void main(String[] args) {
+
+        FindSum findSum = new FindSum();
+
+        long start = System.currentTimeMillis();
+        System.out.println(findSum.findSumUsingMath(99999999));
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time Taken for math function:" + (end - start));
+
+        start = System.currentTimeMillis();
+        System.out.println(findSum.findSumUsingForLoop(99999999));
+        end = System.currentTimeMillis();
+
+        System.out.println("Time taken for loop:" + (end - start));
+
+    }
+
+
+    public int findSumUsingMath(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public int findSumUsingForLoop(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
