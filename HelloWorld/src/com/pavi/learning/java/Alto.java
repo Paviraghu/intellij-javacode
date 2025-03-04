b022a4f77265e5f99cb0548991268448c61a46bb
Paviraghu <paviraghu5410@gmail.com>
2025-03-04 10:23:17 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/Alto.java
@@ -0,0 +1,8 @@
+package com.pavi.learning.java.testedques;
+
+public class Alto extends Car{
+
+    public Alto(String modelName,String colour,int engineCapacity,int numberOfGears,int modelYear){
+        super(modelName,colour,engineCapacity,numberOfGears,modelYear);
+    }
+}
