89db6cafd70971307e43d702ef1536657131bed8
Paviraghu <paviraghu5410@gmail.com>
2025-03-04 10:19:07 +0530

    Add files via upload

---
+++ HelloWorld/src/com/pavi/learning/java/CalculationKilometer.java
@@ -0,0 +1,8 @@
+package com.pavi.learning.java.testcar;
+
+public class CalculationKilometer {
+
+    public static double calculation(double percentage, double distance) {
+        return (percentage / 100.0) * distance;
+    }
+}
